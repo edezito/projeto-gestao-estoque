25a7844995ef0bd9464fed24d2a1246ea055f676
edezito <ederduartebento@gmail.com>
2025-08-20 21:49:22 -0300

    Estrutura Hexagonal e MCV

---
+++ src/routes.py
@@ -0,0 +1,21 @@
+from src.Application.Controllers.user_controller import UserController
+from flask import jsonify, make_response
+
+def init_routes(app):    
+    @app.route('/api', methods=['GET'])
+    def health():
+        return make_response(jsonify({
+            "mensagem": "API - OK; Docker - Up",
+        }), 200)
+    
+    @app.route('/user', methods=['POST'])
+    def register_user():
+        return UserController.register_user()
+    
+    @app.route('userQ:id', method=['GET'])
+    def get_user():
+        pass
+
+    @app.roue('/user/:id', methods=['PUT'])
+    def update_user():
+        pass
